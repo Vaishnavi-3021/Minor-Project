477623f680c142ab58152fc6d14b5846d2cef818
Vaishnavi-3021 <98418598+Vaishnavi-3021@users.noreply.github.com>
2023-04-22 23:31:40 +0530

    Add files via upload

---
+++ minor pro (2)/minor pro/MinorProject1/Starter file - Doclab/Doclab/assets/js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+// preloader
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+/**
+ * MOBILE NAVBAR
+ * show the mobile navbar when click menu button
+ * and hidden after click menu close button or overlay
+ */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+/**
+ * HEADER
+ *
+ * active header when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", activeElementOnScroll);
